6c05ee26a67c39a501f3fb19782fd0b3e03588c8
Sivakumar N <shivask94423@gmail.com>
2025-01-02 11:06:10 +0530

    To get average salary based on each role.

---
+++ Test/testProblem2.js
@@ -0,0 +1,8 @@
+// Find average salary based for each role ( You can find in hr array )
+
+const data = require('../database.js');
+const averageSalaryBasedRole = require('../problem2.js');
+
+const result = averageSalaryBasedRole(data);
+
+console.log(result);
