251e76b150105d21be479eab5063cd4d9b68a05e
Sivakumar N <shivask94423@gmail.com>
2025-01-07 10:13:57 +0530

    function to get average salary based on the role.

--- problem2.js
+++ problem2.js
@@ -1,26 +1,26 @@
+// function to get average salary based on the role.
 function averageSalaryBasedRole(data) {
-    const salaryBasedRole = {};
-    let averagesalary = {};
 
-    for(let details of data.data) {
-        if(salaryBasedRole[details.hr[0]] === undefined) {
-            salaryBasedRole[details.hr[0]] = [details.hr[1]];
-        } else {
-            salaryBasedRole[details.hr[0]].push(details.hr[1]);
+    // geting all salary based role in form of object.
+    const salaryBasedRole = data.data.reduce((accumlater, user) => {
+        let role = user["hr"][0];
+        let salary = user["hr"][1];
+        if (!accumlater[role]) {
+            accumlater[role] = [];
         }
-    }
-    
-    for(let role in salaryBasedRole) {
-        let multiSalary = salaryBasedRole[role];
-        let totalSalary = 0;
+        accumlater[role].push(salary);
+        return accumlater;
+    }, {})
 
-        for(let salary of multiSalary) {
-            salary = parseInt(salary.replace(/[,$]/g, ''));
-            totalSalary += salary;
-        }
-        averagesalary[role] = totalSalary / multiSalary.length;
-    }
-    return averagesalary;
+    // returning average salary based on the role.
+    return Object.keys(salaryBasedRole)
+        .reduce((accumlater, role) => {
+            let salarys = salaryBasedRole[role];
+            let tottalSalary = salarys.reduce((total, salary) => {
+                return total + parseInt(salary.replace(/[$,]/g, ""));
+            }, 0);
+            accumlater[role] = tottalSalary / salarys.length;
+            return accumlater;
+        }, {});
 }
-
 module.exports = averageSalaryBasedRole;
